a4abc5717cbd7d97be585b4dddb631162a35364e
= <=>
2023-08-06 00:09:10 -0400

    nodemon y seed

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -2,6 +2,12 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config');
 
 const User = sequelize.define('user', {
+    id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
     firstName: {
         type: DataTypes.STRING(200),
         allowNull: false
@@ -15,10 +21,10 @@ const User = sequelize.define('user', {
         allowNull: false,
         unique: "users_email_key",
         validate: {
-                isEmail: {
-                    msg: "Email no cumple con el formato requerido (ejemplo@gmail.com)"
-                }
-            },
+            isEmail: {
+                msg: "Email no cumple con el formato requerido (ejemplo@gmail.com)"
+            }
+        },
     },
 }
 );
--- app/scripts/createTables.js
+++ app/scripts/createTables.js
@@ -1,10 +1,20 @@
 const User = require('../models/user.model');
 const sequelize = require('../config/db.config');
 
+
+ const load_user = async () => {
+    console.log('*********CREANDO SEED USER*********')
+    await User.create({firstName: 'Mateo', lastName: 'Díaz', email: 'mateo.diaz@correo.com'});
+    await User.create({firstName: 'Santiago', lastName: 'Mejías', email: 'santiago.mejias@correo.com'});
+    await User.create({firstName: 'Lucas', lastName: 'Rojas', email: 'lucas.rojas@correo.com'});
+    await User.create({firstName: 'Facundo', lastName: 'Fernandez', email: 'facundo.fernandez@correo.com'});
+    return {message: 'Datos Guardados Correctamente'};
+ }
+
 (async () => {
     try {
         await sequelize.sync({ force: true });
-        console.log('Eliminando y resincronizando la base de datos.');
+        console.log(await load_user());
     } catch (error) {
         console.error(error);
     } finally {
